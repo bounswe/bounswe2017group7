7af668dd7c6f560ac17a8445c027b13d40ec426e
Irmak Kavasoglu <irmak.kavasoglu@getir.com>
2017-10-19 19:09:39 +0300

    Added new model ConversationNode

--- bookbotproject/conversationtree/models.py
+++ bookbotproject/conversationtree/models.py
@@ -3,9 +3,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class ConversationNode(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    name = models.CharField(max_length=100, blank=True, default='')
+
+    class Meta:
+        ordering = ('created',)
+
+
 class ConversationTree(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
+    rootNode = models.ForeignKey(ConversationNode, default=0);
 
     class Meta:
         ordering = ('created',)
