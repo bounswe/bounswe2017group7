4da0ee85422b362c9d45ff6b97f754a4b1222ebf
Irmak Kavasoglu <irmakkavasoglu@gmail.com>
2017-10-19 21:19:01 +0300

    Increased intent length of the node model

--- bookbotproject/conversationtree/models.py
+++ bookbotproject/conversationtree/models.py
@@ -8,7 +8,7 @@ class ConversationNode(models.Model):
     #Every node has a title to describe their purpose.
     title = models.CharField(max_length=100, blank=True)
     # Every node has an intent. (Will be used to choose which child the parent will select to go).
-    intent = models.CharField(max_length=50, blank=True)
+    intent = models.CharField(max_length=100, blank=True)
     # Every node has a parent. (Default parent is the root).
     parentNode = models.ForeignKey('self', default=1)
     # Every node has a message that is going to be sent to the user when on that node.
