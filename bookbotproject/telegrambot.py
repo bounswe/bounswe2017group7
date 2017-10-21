8041b081d24143786178dceaa5ea169f353e1c6c
Melih <m.melihmutlu@gmail.com>
2017-10-21 19:56:24 +0300

    typo and bug fix

--- bookbotproject/telegrambot.py
+++ bookbotproject/telegrambot.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 TOKEN = "433004356:AAFzeqBEW8_UgEPDOnJ8bnQAPitaR7gLSSo";
-URL = 	"https://api.telegram.org/not{}/".format(TOKEN);
+URL = 	"https://api.telegram.org/bot{}/".format(TOKEN);
 
 def send_request(url):
 	res = requests.get(url);
@@ -13,18 +13,21 @@ def send_request(url):
 def get_json(url):
 	content = send_request(url);
 	retrieved_json = json.loads(content);
+	print retrieved_json;
 	return retrieved_json;
 
 
 def get_updates():
 	updateURL = URL + "getUpdates"
 	_json = get_json(updateURL);
+	print "updates!!!!!";
+	print _json;
 	return _json;
 
 def get_last_chat(updates):
 	length = len(updates["result"]);
-	text = updates["resutl"][length-1]["message"]["text"];
-	chat_id = updates["result"][length-1]["message"]["id"];
+	text = updates["result"][length-1]["message"]["text"];
+	chat_id = updates["result"][length-1]["message"]["chat"]["id"];
 	return [text, chat_id];
 
 def send_message(chat_id):
