dcdf804d53313473130367975e291053f6f896de
Irmak Kavasoglu <irmakkavasoglu@gmail.com>
2017-10-22 01:10:33 +0300

    Added models to admin page

--- bookbotproject/conversationtree/admin.py
+++ bookbotproject/conversationtree/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import ConversationTree, ConversationNode
 
 # Register your models here.
+admin.site.register(ConversationTree)
+admin.site.register(ConversationNode)
