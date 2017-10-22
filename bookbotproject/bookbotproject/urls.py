d91e3585e5129ea1225604e33bc0612a2810a51b
Ali Goksu Ozkan <aligoksuozkan@Alis-MacBook-Pro.local>
2017-10-23 02:47:55 +0300

    Created httpget for the API connections

--- bookbotproject/bookbotproject/urls.py
+++ bookbotproject/bookbotproject/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from conversationtree import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^nodes/$', views.node_list),
+    url(r'^nodes/(?P<pk>\w+)/$', views.nodes_detail)
 ]
--- bookbotproject/conversationtree/views.py
+++ bookbotproject/conversationtree/views.py
@@ -2,5 +2,55 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from conversationtree.models import Node
+from serializers import NodeSerializer
 
 # Create your views here.
+@csrf_exempt
+def node_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        nodes = Node.objects.all()
+        serializer = NodeSerializer(nodes, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = NodeSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def nodes_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        node = Node.objects.get(name=pk)
+    except Node.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = NodeSerializer(node)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = NodeSerializer(node, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        node.delete()
+        return HttpResponse(status=204)
