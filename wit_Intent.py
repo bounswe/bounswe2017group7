478f2a4d1a6762beeb10aca1cf19c35888ea98f4
Taha Küçükkatırcı <tahakucukkatirci@gmail.com>
2017-10-25 14:16:13 +0300

    Auto_train method addition
    I implemented a method for auto_train by reading from a file.

--- wit_Intent.py
+++ wit_Intent.py
@@ -7,10 +7,28 @@ client = Wit(access_token = access_token)
 def get_Intent(input_text):
 	response = client.message(input_text)
 
-	#Find the intent suggested by Wit
-	intent = response['entities'].keys()[0]
+	intent = None
+	value  = None
+	try:
+		#Find the intent suggested by Wit
+		intent = response['entities'].keys()[0]
 
-	#Find the value in the given text corresponding to that intent
-	value  = response['entities'][intent][0]['value']
+		#Find the value in the given text corresponding to that intent
+		value  = response['entities'][intent][0]['value']
 
+	except:
+		pass
 	return intent,value
+
+
+#given a file containing train sentences line by line, we may train our app on Wit.ai
+def auto_train_by_File(file_):
+	f = open(file_)
+	sentences = f.readlines()
+	f.close()
+	sentences = [sentence[:-1] if sentence.endswith("\n") else sentence for sentence in sentences]
+	for sentence in sentences:
+		client.message(sentence)
+
+
+		
