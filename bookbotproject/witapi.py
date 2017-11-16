06a5230719c7fd4a4f2fd7a894a1bc73935ee9d0
Taha Küçükkatırcı <tahakucukkatirci@Taha-MacBook-Pro.local>
2017-11-16 13:17:18 +0200

    Wit train by templates from database feature added

--- bookbotproject/witapi.py
+++ bookbotproject/witapi.py
@@ -1,9 +1,13 @@
 from wit import Wit
+import json
+import requests
 
 access_token = "4UYTKEFBNBC7PKBSEHZL5KJ3MSKYALZ4"
 client = Wit(access_token = access_token)
 
 
+
+#returns the intent for a given input text
 def get_Intent(input_text):
 	response = client.message(input_text)
 	intent = None
@@ -31,14 +35,76 @@ def auto_train_by_File(file_):
 		client.message(sentence)
 
 
-#This function creates JSON format of intent- template message from our database. 
-#This function will be used to send training data to wit.api
+# this method sends the training data to wit api for it to train the system.
+# data: must be given in proper json format
+# the returned value from "prepare_json_to_train" method will be fed into this as parameter
+def send_data_to_wit(data):
+	url     = 'https://api.wit.ai/samples?v=20170307'
+	headers = {"Authorization": "Bearer 4UYTKEFBNBC7PKBSEHZL5KJ3MSKYALZ4","Content-Type": "application/json" }
+	res = requests.post(url, data=data, headers=headers)
 
-def template_feed_get_json():
-	dict_temps={}
+
+# find the beginning position of a given substring in the given string. we use this for keyword extraction purposes
+def find_str(s, subs):
+    index = 0
+
+    if subs in s:
+        c = subs[0]
+        for ch in s:
+            if ch == c:
+                if s[index:index+len(subs)] == subs:
+                    return index
+
+            index += 1
+
+    return -1
+
+
+# extracts the keyword in a given sentence
+def extract_keyword(sentence):
+	words = sentence.split('*')
+	return words[1].lower().title()
+
+# prepares the json data which is to be sent to wit. 
+# takes lists of three tuples as argument where a tuple = (template_Sentence, intent, keyword)
+def prepare_json_to_train(template_Tuples):
+
+	training_data = []
+
+	for (template, intent, keyword) in template_Tuples:
+		start_of_keyword = find_str(template,keyword)
+		training_data.append({'text': template, 
+					 		  'entities': 
+					 			[
+					 			{'entity':intent,
+					 			 'start':start_of_keyword, 
+					 			  'end' :start_of_keyword + len(keyword),
+					 			  'value':keyword
+					 			}
+					 			]
+					 })
+
+
+	x = json.dumps(training_data)
+
+	return x
+
+# prepares the three tuples to be used for preparation of json_datax
+def prepare_tuples_from_templates(templates):
+	template_Tuples = []
 	for i in range(len(templates)):
-		dict_temps[templates[i].node.intent] = templates[i].template
-	json_data = json.dumps(dict_temps)	
-	return json_data
+		template_Sentence = templates[i].template
+		template_Tuples.append((template_Sentence,templates[i].node.intent,extract_keyword(template_Sentence)))
 
-		
+	return template_Tuples
+
+
+
+t1 = "I want to read *Melih Mutlu*"
+t2 = "Find me some *fantasy* books"
+
+x = prepare_json_to_train(  [(t1,"search_by_author",extract_keyword(t1)),(t2,"search_by_genre",extract_keyword(t2))]  )
+send_data_to_wit(x)
+
+
+
