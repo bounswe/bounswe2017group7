0d3fe5cde81024b01deffe93fedab278d526faf3
Salih Sevgican <sevgicansalih@gmail.com>
2017-12-07 08:38:05 +0300

    bookapi bug fixed

--- bookbotproject/goodReadsApi.py
+++ bookbotproject/goodReadsApi.py
@@ -2,7 +2,7 @@ from goodreads import client
 
 goodreads_key = "UfEAj6yQFcghrsqrZpQ"
 goodreads_secret = "AgAug5etGSAYceT2SeLGBZLMm803I6LiQSxSnGubOjg"
-gc = client.GoodreadsClient(goodreads_key,goodreads_secret)
+gc = client.GoodreadsClient	(goodreads_key,goodreads_secret)
 
 
 """TODO : add bunch of methods like ,search(response): takes response as parameter coming from wit.ai intent and searches .... """
@@ -15,7 +15,9 @@ def search_by_author(intent):
 	return search_books
 def search_by_genre(intent):
 	str = "genre"
+	print("genrenin basi")
 	book = gc.search_books(intent,str)
+	print("search tamamlandi")
 	return book
 def search_by_title(intent):
 	str = "title"
